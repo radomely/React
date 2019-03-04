b3ac9f73b16c7d94f8764b7e8486612b37b81922
Andrii <radomely@gmail.com>
2019-03-04 18:26:02 +0200

    added contact list

--- Module01/lesson01_contact_list/src/EmployeeListItem/EmployeeListItem.jsx
+++ Module01/lesson01_contact_list/src/EmployeeListItem/EmployeeListItem.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import "./EmployeeListItem.css";
 
-const EmployeeListItem = ({title, text}) => {
-    return(
-        <li>
-            <h3>{title}</h3>
-            <p>{text}</p>
-        </li>
-    );
+const EmployeeListItem = ({ title, text }) => {
+  return (
+    <li>
+      <h3>{title}</h3>
+      <p>{text}</p>
+    </li>
+  );
 };
 EmployeeListItem.propTypes = {
-    title: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired
+};
 export default EmployeeListItem;
--- Module01/lesson01_contact_list/src/EmployerInfo/EmployerInfo.jsx
+++ Module01/lesson01_contact_list/src/EmployerInfo/EmployerInfo.jsx
@@ -1,33 +1,33 @@
 import React from "react";
-import UserInfo from "../UserInfo/UserInfo"
-import "./EmployerInfo.css"
+import UserInfo from "../UserInfo/UserInfo";
+import "./EmployerInfo.css";
 import UserList from "../UserList/UserList";
 
 const EmployerInfo = () => {
-    let arr = [
-        {
-            title: 'Call Office',
-            text : '781-000-0002',
-        },
-        {
-            title: 'Call Mobile',
-            text : '617-000-0002',
-        },
-        {
-            title: 'SMS',
-            text : '617-000-0002',
-        },
-        {
-            title: 'Email',
-            text : 'jtaylor@fakemail.com'
-        }
-    ]    
-    return(
-        <div>
-            <UserInfo/>
-            <UserList userData = {arr}/>
-        </div>
-    );
+  let arr = [
+    {
+      title: "Call Office",
+      text: "781-000-0002"
+    },
+    {
+      title: "Call Mobile",
+      text: "617-000-0002"
+    },
+    {
+      title: "SMS",
+      text: "617-000-0002"
+    },
+    {
+      title: "Email",
+      text: "jtaylor@fakemail.com"
+    }
+  ];
+  return (
+    <div>
+      <UserInfo />
+      <UserList userData={arr} />
+    </div>
+  );
 };
 
 export default EmployerInfo;
