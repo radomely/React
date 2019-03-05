7bcd293c87aefb0ddec05ce61eab7f47380f6af9
Andrii <radomely@gmail.com>
2019-03-05 18:30:11 +0200

    added state

--- Module02/state/src/BasketItem/BasketItem.jsx
+++ Module02/state/src/BasketItem/BasketItem.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import './BasketItem.css'
-import PropTypes from 'prop-types';
+import React from "react";
+import "./BasketItem.css";
+import PropTypes from "prop-types";
 
-const BasketItem = ({cartOne, removefromCart}) => {
-    return (
-        <li>
-            <p className="card__title">{cartOne.title}</p>
-            <p className="price">{cartOne.price} UAH</p>
-            <p className="BasketButton" data-name={cartOne.title} onClick={removefromCart}>X</p>
-        </li>
-    );
+const BasketItem = ({ cartOne, removefromCart }) => {
+  return (
+    <li>
+      <p className="card__title">{cartOne.title}</p>
+      <p className="price">{cartOne.price} UAH</p>
+      <p
+        className="BasketButton"
+        data-name={cartOne.title}
+        onClick={removefromCart}
+      >
+        X
+      </p>
+    </li>
+  );
 };
 
-BasketItem.propTypes = {
-    
-};
+BasketItem.propTypes = {};
 
 export default BasketItem;
